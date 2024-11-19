7082db005a1d5d22938a697db36a9c382facd53a
naoki <n_mgem6_17@yahoo.co.jp>
2024-11-19 21:35:13 +0900

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES
+    ? '/react-router-test/' // レポジトリ名を設定
+    : './',
 })
