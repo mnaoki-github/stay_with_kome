9d2e0a8fb5f22b633b4de919ab3e40eef63e7bfd
naoki <n_mgem6_17@yahoo.co.jp>
2024-12-21 19:57:00 +0900

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build : {
+    outDir: './dist' //build出力先
+  },
   base: process.env.GITHUB_PAGES
-    // ? '/stay_with_kome/' // レポジトリ名を設定
-    ? './' // レポジトリ名を設定
-    : '/',
+    ? '/stay_with_kome/' // レポジトリ名を設定
+    :  process.env.NODE_ENV === 'production'
+    ?  '/'  //本番環境('preview')
+    :  './', //開発環境('dev')
 })
