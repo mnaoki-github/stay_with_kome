d8470c26cf2b7506924f5fe31b37c71731295fb8
naoki <n_mgem6_17@yahoo.co.jp>
2025-01-03 18:17:21 +0900

    add globalCSS

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -5,7 +5,7 @@ export default function Header (){
     return (
         <header>
             <div>
-                <img src="https://res.cloudinary.com/daoj4u56z/image/upload/v1734955130/hero_image_foctaj.jpg" alt="トップ画像" />
+                <img src="https://res.cloudinary.com/daoj4u56z/image/upload/c_pad,w_2000/v1734955130/hero_image_foctaj.jpg" alt="トップ画像" />
                 <HeaderNav/>
             </div>
         </header>
